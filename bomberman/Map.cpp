5689d19b2d3fa2a918424224d8d65933488435a7
tomatRe <angelre.rebollo@gmail.com>
2022-07-04 10:57:07 +0200

    Implemented map .cpp & .h

--- bomberman/Map.cpp
+++ bomberman/Map.cpp
@@ -1,1 +1,64 @@
 #include "Map.h"
+
+Map::Map()
+{
+}
+
+Map::Map(std::string mapName, SDL_Texture* tileSet)
+{
+	LoadMap(mapName);
+	this->tileSet = tileSet;
+}
+
+int ** Map::LoadMap(std::string mapName)
+{
+	std::stringstream ss;
+	std::string line;
+
+	// Read from the text file
+	std::ifstream mapFile(mapName);
+
+	int i = 0;
+	while (std::getline(mapFile, line)) {
+		// Output the text from the file
+
+		for (int j = 0; j < line.length(); j++)
+		{
+			//Get tile position on the string
+			char sTile = line.at(j);
+			int tile;
+
+			//Convert char to string
+			ss << sTile;
+			//Convert string to int
+			ss >> tile;
+
+			//save int to the array
+			mapTiles[i][j] = tile;
+		}
+
+		i++;
+	}
+
+	// Close the file
+	mapFile.close();
+
+	return mapTiles;
+}
+
+SDL_Texture * Map::GetSpriteAtLocation(int x, int y)
+{
+	return nullptr;
+}
+
+void Map::SetSpriteAtLocation(int x, int y)
+{
+}
+
+void Map::ReadMap(std::string mapName)
+{
+}
+
+Map::~Map()
+{
+}
--- bomberman/Map.h
+++ bomberman/Map.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include <SDL_image.h>
 
 class Map
 {
 	//Every tile are 4 pixels on screen
 	public:
+		Map();
+		Map(std::string mapName, SDL_Texture* tileSet);
+		~Map();
 
+		int** LoadMap(std::string mapName);
+		SDL_Texture* GetSpriteAtLocation(int x, int y);
+		void SetSpriteAtLocation(int x, int y);
 	private:
-		static int mapTiles[320][180];
+		void ReadMap(std::string mapName);
+
+		SDL_Texture* tileSet;
+		int** mapTiles;
 };
 
